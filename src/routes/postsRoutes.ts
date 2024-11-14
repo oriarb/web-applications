a0172b1a0bfad2497efda0b3d4b88d7c75bdd7c0
oriarb <oriarb@gmail.com>
2024-11-14 19:50:30 +0200

    add posts api

--- src/routes/postsRoutes.ts
+++ src/routes/postsRoutes.ts
@@ -1,6 +1,14 @@
 import {Router} from "express";
+import {createPost, getAllPosts, getPostById, updatePost} from "../controllers/posts";
 
 const router = Router();
 
+router.get('/', getAllPosts);
+
+router.get('/:id', getPostById);
+
+router.post('/', createPost);
+
+router.put('/:id', updatePost);
 
 export default router;
